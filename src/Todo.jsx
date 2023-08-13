d3c77c310f12d3a869ae658f3547b715bb713ccd
webdev-ashishk <webdev.ashishk@gmail.com>
2023-08-13 12:17:46 +0530

    basic leve of todo application completed

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,16 +2,35 @@ import { useState } from "react";
 
 const Todo = () => {
   const [todo, setTodo] = useState("");
-  const [listData, setListData] = useState([]);
+  const [listDatas, setListData] = useState([]);
   function handleChange(e) {
     setTodo(e.target.value);
     // console.log(todo);
   }
   function addTodo() {
     //...listData copy all the data form the array
-    setListData([...listData, todo]);
-    console.log(todo);
-    setTodo("");
+    // Issue is after double click todo added to list !
+    // Why this problem arise becoz of asynchronous nature of state .
+    // setListData([...listData, todo]);
+    // console.log(listData);
+
+    // solve the above issue
+    // this will work synchronously
+    setListData((listData) => {
+      const updatedList = [...listData, todo];
+      console.log(updatedList);
+      setTodo("");
+      return updatedList;
+    });
+  }
+  function handleRemoteTodo(index) {
+    const updatedListData = listDatas.filter((element, id) => {
+      return index != id;
+    });
+    setListData(updatedListData);
+  }
+  function handleRemoveListDatas() {
+    setListData([]);
   }
   return (
     <>
@@ -23,7 +42,24 @@ const Todo = () => {
           value={todo}
         />
         <button onClick={addTodo}>add Todo</button>
+        {listDatas.map((listData, index) => {
+          return (
+            <>
+              <ul key={index}>
+                <li>
+                  {listData}{" "}
+                  <button onClick={() => handleRemoteTodo(index)}>
+                    remove
+                  </button>
+                </li>
+              </ul>
+            </>
+          );
+        })}
       </div>
+      {listDatas.length > 0 && (
+        <button onClick={handleRemoveListDatas}>remove-All</button>
+      )}
     </>
   );
 };
